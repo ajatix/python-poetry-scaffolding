3903c3d2dc4fde1fb77ae385c01f90207eeda2d2
Ajay Viswanathan <ajay.viswanathan@gmail.com>
2021-08-22 16:39:23 +0700

    Fix unit test and venv true by default

--- {{cookiecutter.package}}/src/test/python/test_{{cookiecutter.package}}.py
+++ {{cookiecutter.package}}/src/test/python/test_{{cookiecutter.package}}.py
@@ -1,6 +1,6 @@
 from {{cookiecutter.package}} import __version__, __python_version__
 
 
-def test_{{cookiecutter.package}}:
+def test_{{cookiecutter.package}}():
     assert __version__ == '{{cookiecutter.version}}'
     assert __python_version__ == '{{cookiecutter.python_version}}'
